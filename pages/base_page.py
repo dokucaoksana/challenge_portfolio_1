c9b6a4e4f10db34ab3200f16cd04e9c28bf75481
dokucaoksana <dokucaoksana@gmail.com>
2023-02-04 19:38:24 +0200

    Refactor

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,11 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from utils.settings import DEFAULT_LOCATOR_TYPE
 
 
 class BasePage():
@@ -17,4 +23,7 @@ class BasePage():
         self.driver.get(url)
         return self.driver.title
 
-
+    def wait_for_element_to_be_clickable(self, locator, locator_type=DEFAULT_LOCATOR_TYPE):
+        wait = WebDriverWait(self.driver, 5)
+        element = wait.until(EC.element_to_be_clickable((locator_type, locator)))
+        time.sleep(3)
